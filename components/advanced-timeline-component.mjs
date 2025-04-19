e713ebdeb3064c619e2a22ecfd249c6e452b0af7
yashrajbharticybtekk <y.bharti@cybtekk.com>
2025-04-20 02:30:37 +0530

    advance card

--- components/advanced-timeline-component.mjs
+++ components/advanced-timeline-component.mjs
@@ -22,7 +22,7 @@ export class AdvancedTimeline extends HTMLElement {
 
     this.shadowRoot.innerHTML = `
           <style>
-            article {
+            article.container {
               max-inline-size: max-content;
               inline-size: 90%;
               margin-inline: auto;
@@ -32,16 +32,30 @@ export class AdvancedTimeline extends HTMLElement {
               justify-content: center;
               align-items: start;
               gap: 0 25px;
+
+              & md-chip-set{
+                margin-block-end: 15px;
+              }
             }
     
             .timeline {
-              padding-inline: 40px;
-              line-height: 5;
               border-inline-start: 2px solid var(--md-sys-color-primary-container);
     
               & > .info {
                 list-style: none;
                 position: relative;
+                & > article {
+                position: relative;
+                background-color: color-mix(in srgb, var(--md-sys-color-primary-container) 20%, transparent 80%);
+                padding: 30px;
+                margin-block: 40px;
+                border-radius: 24px;
+                  & > div {
+                    display: flex;
+                    align-items: center;
+                    gap: 12px;
+                  }
+                }
               }
     
               & > .info::after {
@@ -57,16 +71,53 @@ export class AdvancedTimeline extends HTMLElement {
               }
             }
           </style>
-          <article>
+          <article class="container">
             <h2>${title} - ${experiences.highest_level}</h2>
             <ul class="timeline">
               ${
                 degrees
                   ? degrees
                       .map(
-                        (degree) => `
+                        (education) => `
                 <li class="info">
-                ${degree}
+                      <article class="education-card">
+                          <h3>${education.degree}</h3>
+                          ${
+                            education.isTop50 || education.isTop25
+                              ? `<md-chip-set>
+                              <md-assist-chip label="Top School"></md-assist-chip>
+                          </md-chip-set>`
+                              : ""
+                          }
+                          <div class="time">
+                              <md-icon-button>
+                                  <md-icon>date_range</md-icon>
+                              </md-icon-button>
+                              <p>${education.startDate || "N/A"} - ${
+                          education.endDate || "N/A"
+                        }</p>
+                          </div>
+                          <div class="grades">
+                              <md-icon-button>
+                                  <md-icon>star</md-icon>
+                              </md-icon-button>
+                              <p>${education.gpa}</p>
+                          </div>
+                          <div class="school">
+                              <md-icon-button>
+                                  <md-icon>school</md-icon>
+                              </md-icon-button>
+                              <div><p>${education.originalSchool}</p>
+                              <p>(${education.school})</p></div>
+                          </div>
+                          <div class="subject">
+                              <md-icon-button>
+                                  <md-icon>book_2</md-icon>
+                              </md-icon-button>
+                              <p>${education.subject || "N/A"}</p>
+                          </div>  
+                          <md-ripple></md-ripple>
+                      </article>
                 </li>
               `
                       )
