c6c65cd0893dd932507c6daeb8d5abc01aed8dcf
yashrajbharti <hrithik.yashrajbharti@gmail.com>
2025-10-08 02:19:57 +0530

    Add Jest setup file

---
+++ jest.setup.js
@@ -0,0 +1,20 @@
+// Jest setup file for frontend tests
+// Add any global test setup here
+
+// Mock window.scrollIntoView for tests
+Element.prototype.scrollIntoView = jest.fn();
+
+// Mock window.matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
